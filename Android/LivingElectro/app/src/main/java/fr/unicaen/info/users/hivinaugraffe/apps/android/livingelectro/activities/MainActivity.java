3d9366026d4c3fa6728593b96cee1e7b68d05590
hivinau.graffe <h.graffe@futurdigital.fr>
2017-05-21 22:19:52 +0200

    Initial commit

--- Android/LivingElectro/app/src/main/java/fr/unicaen/info/users/hivinaugraffe/apps/android/livingelectro/activities/MainActivity.java
+++ Android/LivingElectro/app/src/main/java/fr/unicaen/info/users/hivinaugraffe/apps/android/livingelectro/activities/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import fr.unicaen.info.users.hivinaugraffe.apps.android.livingelectro.R;
+import fr.unicaen.info.users.hivinaugraffe.apps.android.livingelectro.adapters.PartsAdapter;
 
 /**
  * Created by ios_developer on 21/05/2017.
@@ -16,6 +17,7 @@ public final class MainActivity extends AppCompatActivity {
 
     private TabLayout tabLayout = null;
     private ViewPager viewPager = null;
+    private PartsAdapter adapter = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,9 +30,17 @@ public final class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        adapter = new PartsAdapter(getSupportFragmentManager());
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
 
+        viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager, true);
     }
 
--- Android/LivingElectro/app/src/main/java/fr/unicaen/info/users/hivinaugraffe/apps/android/livingelectro/adapters/PartsAdapter.java
+++ Android/LivingElectro/app/src/main/java/fr/unicaen/info/users/hivinaugraffe/apps/android/livingelectro/adapters/PartsAdapter.java
@@ -1,8 +1,15 @@
 package fr.unicaen.info.users.hivinaugraffe.apps.android.livingelectro.adapters;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.unicaen.info.users.hivinaugraffe.apps.android.livingelectro.fragments.Songs;
+import fr.unicaen.info.users.hivinaugraffe.apps.android.livingelectro.models.Part;
 
 /**
  * Created by ios_developer on 21/05/2017.
@@ -10,17 +17,59 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 
 public class PartsAdapter extends FragmentStatePagerAdapter {
 
+    private final List<Part> parts;
+
     public PartsAdapter(FragmentManager fm) {
         super(fm);
+
+        parts = new ArrayList<>();
+    }
+
+    public boolean addPart(Part part) {
+
+        boolean state = parts.add(part);
+
+        notifyDataSetChanged();
+
+        return state;
+    }
+
+    public boolean removePart(Part part) {
+
+        boolean state = parts.remove(part);
+
+        notifyDataSetChanged();
+
+        return state;
+    }
+
+    public void removeAllParts() {
+
+        parts.clear();
+        notifyDataSetChanged();
     }
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+
+        Bundle arguments = new Bundle();
+
+        Part part = parts.get(position);
+        arguments.putParcelableArray(Part.class.getName(), part.getSongs());
+
+        Songs songs = new Songs();
+        songs.setArguments(arguments);
+
+        return songs;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return parts.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return parts.get(position).getGenre();
     }
 }
